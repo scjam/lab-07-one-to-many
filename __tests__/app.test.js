63031d03ee29e7854c8da00b87e720488eccb550
Sydney James <sydneyjames7@gmail.com>
2020-12-09 17:13:31 -0800

    first endpoint complete and test passes

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -1,10 +1,31 @@
+const fs = require('fs');
 const request = require('supertest');
 const app = require('../lib/app');
+const pool = require('../lib/utils/pool');
 
 describe('endpoints', () => {
-  it('creates a new movie via POST', async() => {
-    const response = await request(app).get('/');
+  beforeAll(() => {
+    return pool.query(fs.readFileSync('./sql/setup.sql', 'utf-8'));
+  });
 
-    expect(response.body).toEqual({ foo: 'bar' });
+  afterAll(() => {
+    return pool.end();
+  });
+
+  it('creates a new movie via POST', async() => {
+    const res = await request(app)
+      .post('/movies')
+      .send({
+        title: 'Titanic',
+        director: 'James Cameron',
+        year: '1997'
+      });
+
+    expect(res.body).toEqual({
+      id: '1',
+      title: 'Titanic',
+      director: 'James Cameron',
+      year: 1997
+    });
   });
 });
---
+++ sql/setup.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS movies;
+
+CREATE TABLE movies (
+    id BIGINT GENERATED ALWAYS AS IDENTITY PRIMARY KEY,
+    title TEXT NOT NULL,
+    director TEXT NOT NULL,
+    year INT NOT NULL
+);
